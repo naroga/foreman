8234a1c367c2c7a2d37a8bbf218cd54c19daf5c8
Pedro Cordeiro <pedro@naroga.com.br>
2015-08-07 11:55:48 -0300

    Fixes #5: Webserver started by the foreman:processor:start command. Already listens to requests.

--- src/AppBundle/Processor/Processor.php
+++ src/AppBundle/Processor/Processor.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Processor;
 
+use React\EventLoop\Factory;
+use React\EventLoop\LoopInterface;
+use React\Socket\Server;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
@@ -10,11 +13,63 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class Processor
 {
+
+    /** @var int */
+    protected $interval;
+
+    /** @var int */
+    protected $port = 3440;
+
+    /** @var LoopInterface */
+    protected $loop;
+
+    /**
+     * Class constructor
+     *
+     * @param array $processorConfiguration Configuration array, parsed from app/config/services.yml
+     */
+    public function __construct(array $processorConfiguration)
+    {
+        $this->interval = $processorConfiguration['interval'];
+        $this->port = $processorConfiguration['port'];
+    }
+
+    /**
+     * Starts the processor server.
+     *
+     * @param OutputInterface $output
+     * @throws \React\Socket\ConnectionException
+     */
     public function start(OutputInterface &$output)
     {
-        $output->writeln('<info>Starting server...</info>');
-        while (true) {
-            sleep(1);
-        }
+        $this->loop = Factory::create();
+        $socket = new Server($this->loop);
+
+        $http = new \React\Http\Server($socket);
+        $http->on('request', [$this, 'manageRequest']);
+
+        $socket->listen($this->port);
+
+        $this->loop->addPeriodicTimer($this->interval, [$this, 'manageQueue']);
+        $this->loop->run();
+    }
+
+    /**
+     * Manages a request, made to the Http server.
+     *
+     * @param $request
+     * @param $response
+     */
+    public function manageRequest($request, $response)
+    {
+        //Stub
+    }
+
+    /**
+     * Manages the queue. Dispatches processes, kills timed out processes, etc.
+     */
+    public function manageQueue()
+    {
+        //Stub
     }
 }
